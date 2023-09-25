d165f086295fa853b2450d6774fcb6337354551c
cicadago <143706582+cicadago@users.noreply.github.com>
2023-09-25 18:55:57 +0800

    Update tutor_python01.py

--- tutor_python01.py
+++ tutor_python01.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Enable interactive mode
+plt.interactive(True)
+
 x = np.linspace(0, 2 * np.pi, 200)  # generates 200 evenly spaced points between 0 and 2π
 print(type(x), x.shape) # <class 'numpy.ndarray'> (200,)
 y = np.sin(x)
